1e0a1c88a49e7137d1bfcde115df1a494b115ffc
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-03 15:06:19 -0300

    setting project

--- first-node-project/backend/src/index.js
+++ first-node-project/backend/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-  return response.send('Hello World');
+  return response.json({message: 'Hello World'});
 })
 
 app.listen(3333);
