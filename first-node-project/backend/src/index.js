be10105f212b56c24d5dd21e07a2c5a3cbd84fe2
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-03 14:57:20 -0300

    creating simple request routes

--- first-node-project/backend/src/index.js
+++ first-node-project/backend/src/index.js
@@ -1,4 +1,10 @@
+const { request } = require('express');
 const express = require('express');
 
 const app = express();
+
+app.get('/projects', (request, response) => {
+  return response.send('Hello World');
+})
+
 app.listen(3333);
