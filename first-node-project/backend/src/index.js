8d3ce6003bad960889aa77df33c5eb41d2a12128
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-03 15:27:21 -0300

    scriptis package.json

--- first-node-project/backend/src/index.js
+++ first-node-project/backend/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-  return response.json({message: 'Hello World'});
-})
+  return response.json({message: 'Hello Gostack'});
+});
 
 app.listen(3333);
