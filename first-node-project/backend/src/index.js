1d3dc22c24f308407749fd0a9354b46b85f4fa4b
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-05 20:21:13 -0300

    json return projects

--- first-node-project/backend/src/index.js
+++ first-node-project/backend/src/index.js
@@ -15,7 +15,10 @@ const app = express();
  */
 
 app.get('/projects', (request, response) => {
-  return response.json({message: 'Hello Gostack'});
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
 });
 
 app.listen(3333, () => {
