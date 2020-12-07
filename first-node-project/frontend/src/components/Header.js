914cf0cd6881d012ab94a9c02b25fd825dc78bfb
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 20:15:27 -0300

    20:15:27 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/src/components/Header.js
+++ first-node-project/frontend/src/components/Header.js
@@ -1,9 +1,10 @@
-import React, { Children } from 'react';
+import React from 'react';
 
 const Header = ({title, children} ) => {
   return (
     <header>
       <h1>{title}</h1>
+      {children}
     </header>
   );
 }
