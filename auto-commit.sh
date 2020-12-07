03134757810c3a47f2243eff1f0b383e4998c211
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-07 14:58:41 -0300

    14:58:41 - Bootcamp RocketSeat - Autocommit

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,5 +1,11 @@
-now=$(date +"%T")
-echo "Current time : $now"
-git add .
-git commit -m "$now - Bootcamp RocketSeat - Autocommit"
-git push
+
+while :
+do
+  sleep 5 &
+  now=$(date +"%T")
+  echo "Current time : $now"
+  git add .
+  git commit -m "$now - Bootcamp RocketSeat - Autocommit"
+  git push
+  wait
+done
