ce875264df4a835fd917d532ea59f5cf37166db0
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 19:03:52 -0300

    19:03:52 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/src/index.js
+++ first-node-project/frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import App from './App';
+
 //JSX => HTML dentro do JavaScript (Javascript XML)
 render(<h1>Hello World</h1>, document.getElementById('app'));
