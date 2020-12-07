ba218a73b2b1479d94ccea3417d3c7b3a90adef3
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 15:30:12 -0300

    15:30:12 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/webpack.config.js
+++ first-node-project/frontend/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js')
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
 }
