d01f2abf0a97cd2411f131e3eeaf5e285c550f6a
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 19:05:08 -0300

    19:05:08 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/src/App.js
+++ first-node-project/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /**
- * Sempre criaremos um componente quando queremos reaproveitar um código
+ * Sempre criaremos um componente quando queremos reaproveitar um código, em vários lugares de uma aplicação.
  */
 
 function App() {
