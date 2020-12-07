2a4b9c9a005fc6f0fe41644c42d07add6672f946
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 15:10:16 -0300

    15:10:16 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/public/script.js
+++ first-node-project/frontend/public/script.js
@@ -1,4 +1,4 @@
 /**
  * Babel: Converter (transpilar) código do React para um código que o browser entenda.
- * Webpack: Para cada tipo  de arquivo  (.js, .css, .png) será convertido de uma maneira 
+ * Webpack: Para cada tipo  de arquivo  (.js, .css, .png) será convertido de uma maneira diferente
  */
