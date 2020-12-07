a15fc7701c5f42b8f6ccbbefdf0330a86b718568
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 16:39:29 -0300

    16:39:29 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/public/script.js
+++
@@ -1,7 +0,0 @@
-/**
- * Babel: Converter (transpilar) código do React para um código que o browser entenda.
- * Webpack: Para cada tipo  de arquivo  (.js, .css, .png) será convertido de uma maneira diferente
- * 
- * Loaders: babel-loader, css-loader, image-loader
- * O webpack utiliza os loaders para que nosso javascript possa desempenhar mais atividades.
- */
