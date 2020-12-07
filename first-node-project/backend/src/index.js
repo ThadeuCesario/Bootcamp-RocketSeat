4dd8e5e831384483cdeb8dccf10f19c26323cd8a
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-07 12:11:33 -0300

    fixing put method

--- first-node-project/backend/src/index.js
+++ first-node-project/backend/src/index.js
@@ -53,20 +53,21 @@ app.post('/projects', (request, response) => {
 
 app.put('/projects/:id', (request, response) => {
   const {id} = request.params;
+  const {title, owner} = request.body;
   const projectIndex = projects.findIndex(project => project.id === id);
   
   if(projectIndex < 0) {
     return response.status(400).json({error: 'Project Not Found'});
   }
 
+  const project = {
+    id,
+    title,
+    owner,
+  }
 
-  console.log("project", project);
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 5',
-  ]);
+  projects[projectIndex] = project;
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
