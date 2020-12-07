aa7249c8c5a7bbfd6a8d6e4a943542785894749e
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 15:05:45 -0300

    15:05:45 - Bootcamp RocketSeat - Autocommit

--- auto-commit.sh
+++ auto-commit.sh
@@ -3,9 +3,9 @@ while :
 do
   sleep 5 &
   now=$(date +"%T")
-  echo "Current time : $now"
   git add .
   git commit -m "$now - Bootcamp RocketSeat - Autocommit"
   git push
+  echo "Autocommit finished."
   wait
 done
