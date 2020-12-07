fb178af8d7ea51e60d80519ad25615683d85c5d3
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-07 19:58:20 -0300

    19:58:20 - Bootcamp RocketSeat - Autocommit

--- first-node-project/frontend/src/App.js
+++ first-node-project/frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 
 /**
  * Sempre criaremos um componente quando queremos reaproveitar um código, em vários lugares de uma aplicação.
+ * Lembre-se que no React não são permitidos elementos adjacentes.
  */
 
 function App() {
